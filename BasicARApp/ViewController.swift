56e960097619ffbf6c856318075995c5aaeaad6a
Alejandro Hidalgo Badillo <alejandrohidalgobadillo@gmail.com>
2022-10-18 12:05:17 -0500

    Initial Commit

---
+++ BasicARApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BasicARApp
+//
+//  Created by Alejandro Hidalgo Badillo on 18/10/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
